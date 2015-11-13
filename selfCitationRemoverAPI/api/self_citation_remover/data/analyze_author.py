9d15dbe1798adb59dde05aa3fc6b7f40194b9eb3
mathlover777 <souravmathlover@gmail.com>
2015-11-13 23:33:43 +0530

    all working

--- selfCitationRemoverAPI/api/self_citation_remover/data/analyze_author.py
+++ selfCitationRemoverAPI/api/self_citation_remover/data/analyze_author.py
@@ -56,9 +56,9 @@ def compute_collaborative_citation(paper,collab_set):
 		count = 0
 		for citing_paper in paper:
 			citing_author_set = set(paper[citing_paper])
-			print citing_author_set
-			print collab_set
-			print '************************'
+			# print citing_author_set
+			# print collab_set
+			# print '************************'
 			if len(citing_author_set.intersection(collab_set)) > 0:
 				count += 1
 		return count
@@ -77,97 +77,142 @@ def compute_own_citation(author_name,paper):
 	return count
 
 
-def plot_graphs(citation_detail):
-	own_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["own_citation"]) for x in citation_detail]
-	self_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["self_citation"]) for x in citation_detail]
-	collab_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["collaborative_citation"]) for x in citation_detail]
-	
+def plot_separate_graphs(citation_detail_dict):
+	for author_name in citation_detail_dict:
+		citation_detail = citation_detail_dict[author_name]
+		own_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["own_citation"]) for x in citation_detail]
+		self_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["self_citation"]) for x in citation_detail]
+		collab_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["collaborative_citation"]) for x in citation_detail]
+		
 
-	own_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,own_citation)
-	self_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,self_citation)
-	collab_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,collab_citation)
+		own_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,own_citation)
+		self_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,self_citation)
+		collab_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,collab_citation)
 
-	X_own = map(lambda x:x[0],own_citation)
-	Y_own = map(lambda y:y[1],own_citation)
+		X_own = map(lambda x:x[0],own_citation)
+		Y_own = map(lambda y:y[1],own_citation)
 
-	X_self = map(lambda x:x[0],self_citation)
-	Y_self = map(lambda y:y[1],self_citation)
+		X_self = map(lambda x:x[0],self_citation)
+		Y_self = map(lambda y:y[1],self_citation)
 
-	X_collab = map(lambda x:x[0],collab_citation)
-	Y_collab = map(lambda y:y[1],collab_citation)
-
-
-	max_X = max(X_own + X_self + X_collab)
-	max_Y = max(Y_own + Y_self + Y_collab)
-
-	fit_self = np.polyfit(X_self,Y_self,3)
-	fit_fn_self = np.poly1d(fit_self) 
-	# plt.plot(X_self,Y_self,'r^',sorted(X_self),fit_fn_self(sorted(X_self)),'r')
-	# plt.ylim([0,max_Y])
-	# # plt.show()
-	# plt.savefig('self.eps', format='eps', dpi=1000)
+		X_collab = map(lambda x:x[0],collab_citation)
+		Y_collab = map(lambda y:y[1],collab_citation)
 
 
+		max_X = max(X_own + X_self + X_collab)
+		max_Y = max(Y_own + Y_self + Y_collab)
 
-	fit_own = np.polyfit(X_own,Y_own,3)
-	fit_fn_own = np.poly1d(fit_own)
-	plt.plot(X_own,Y_own,'g^',sorted(X_own),fit_fn_own(sorted(X_own)),'g')
-	plt.ylim([0,max_Y])
-	# plt.show()
-	plt.savefig('own.eps', format='eps', dpi=1000)
+		fit_self = np.polyfit(X_self,Y_self,3)
+		fit_fn_self = np.poly1d(fit_self) 
+
+		fit_own = np.polyfit(X_own,Y_own,3)
+		fit_fn_own = np.poly1d(fit_own)
+
+		fit_collab = np.polyfit(X_collab,Y_collab,3)
+		fit_fn_collab = np.poly1d(fit_collab)
+
+		plt.plot(X_self,Y_self,'r^',label = 'self')
+		plt.plot(X_own,Y_own,'g^',label = 'own')
+		plt.plot(X_collab,Y_collab,'b^',label = 'collaborative')
+		plt.plot(sorted(X_self),fit_fn_self(sorted(X_self)),'r')
+		plt.plot(sorted(X_own),fit_fn_own(sorted(X_own)),'g')
+		plt.plot(sorted(X_collab),fit_fn_collab(sorted(X_collab)),'b')
+		plt.legend(loc='upper left')
+		plt.ylim([0,max_Y])
+		file_name = author_name.replace(' ','').replace('.','')
+		plt.savefig(file_name + '_all.eps', format='eps', dpi=1000)
+		plt.cla()
 
 
-	fit_collab = np.polyfit(X_collab,Y_collab,3)
-	fit_fn_collab = np.poly1d(fit_collab)
-	plt.plot(X_collab,Y_collab,'b^',sorted(X_collab),fit_fn_collab(sorted(X_collab)),'b')
-	plt.ylim([0,max_Y])
-	# plt.show()
-	plt.savefig('collab.eps', format='eps', dpi=1000)
+	return
 
-	plt.plot(X_self,Y_self,'r^',label = 'self')
-	plt.plot(X_own,Y_own,'g^',label = 'own')
-	plt.plot(X_collab,Y_collab,'b^',label = 'collaborative')
-	plt.plot(sorted(X_self),fit_fn_self(sorted(X_self)),'r')
-	plt.plot(sorted(X_own),fit_fn_own(sorted(X_own)),'g')
-	plt.plot(sorted(X_collab),fit_fn_collab(sorted(X_collab)),'b')
-	plt.legend(loc='upper left')
+def plot_together_graphs(citation_detail_dict):
+	max_X = -1
+	max_Y = -1
+	style_list = ['^','o']
+	line_style_list = ['','--']
+	i = 0
+	for author_name in citation_detail_dict:
+		citation_detail = citation_detail_dict[author_name]
+		own_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["own_citation"]) for x in citation_detail]
+		self_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["self_citation"]) for x in citation_detail]
+		collab_citation = [(citation_detail[x]["total_citation"],citation_detail[x]["collaborative_citation"]) for x in citation_detail]
+		
+
+		own_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,own_citation)
+		self_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,self_citation)
+		collab_citation = filter(lambda x: x[0] >= 0 and x[1] >= 0,collab_citation)
+
+		X_own = map(lambda x:x[0],own_citation)
+		Y_own = map(lambda y:y[1],own_citation)
+
+		X_self = map(lambda x:x[0],self_citation)
+		Y_self = map(lambda y:y[1],self_citation)
+
+		X_collab = map(lambda x:x[0],collab_citation)
+		Y_collab = map(lambda y:y[1],collab_citation)
+
+
+		max_X = max(max_X,max(X_own + X_self + X_collab))
+		max_Y = max(max_Y,max(Y_own + Y_self + Y_collab))
+
+		fit_self = np.polyfit(X_self,Y_self,3)
+		fit_fn_self = np.poly1d(fit_self) 
+
+		fit_own = np.polyfit(X_own,Y_own,3)
+		fit_fn_own = np.poly1d(fit_own)
+
+		fit_collab = np.polyfit(X_collab,Y_collab,3)
+		fit_fn_collab = np.poly1d(fit_collab)
+
+		style = style_list[i]
+		line_style = line_style_list[i]
+
+		plt.plot(X_self,Y_self,'r' + style,label = 'self-' + author_name)
+		plt.plot(X_own,Y_own,'g' + style,label = 'own-' + author_name)
+		plt.plot(X_collab,Y_collab,'b' + style,label = 'collaborative-' + author_name)
+		plt.plot(sorted(X_self),fit_fn_self(sorted(X_self)),'r' + line_style)
+		plt.plot(sorted(X_own),fit_fn_own(sorted(X_own)),'g' + line_style)
+		plt.plot(sorted(X_collab),fit_fn_collab(sorted(X_collab)),'b' + line_style)
+		plt.legend(loc='upper left',prop={'size':10	})
+		i += 1
 	plt.ylim([0,max_Y])
 	plt.savefig('all.eps', format='eps', dpi=1000)
-
-
 	return
 
-def analyze_author(author_name):
-	paper_folder_list = filter(lambda x:x not in file_black_list,listdir(author_name))
-	paper_dict = {}
-	for paper in paper_folder_list:
-		paper_dict[normalize_paper_name(paper)] = {}
-		citing_list = filter(lambda x:x not in file_black_list,listdir(author_name + "/" + paper))
-		for citing_paper in citing_list:
-			paper_dict[normalize_paper_name(paper)][normalize_paper_name(citing_paper)] = get_author_list(author_name + "/" + paper + "/" + citing_paper)
-	# pprint.pprint(paper_dict)
-	collab_set,side_author_dict = get_collaboration_set(author_name)
-
-	# print collab_set
-
-	# print side_author_dict
-	# pprint.pprint(side_author_dict.keys())
+def analyze_author(author_name_list):
 	citation_detail = {}
-	for paper in paper_dict:
-		citation_detail[paper] = {}
-		citation_detail[paper]["total_citation"] = len(paper_dict[paper])
-		try:
-			citation_detail[paper]["self_citation"] = compute_collaborative_citation(paper_dict[paper],side_author_dict[paper])
-		except Exception as e:
-			citation_detail[paper]["self_citation"] = -1
-			# print str(e)
-			print 'in main loop = {' + paper + '}'
-		citation_detail[paper]["own_citation"] = compute_own_citation(author_name,paper_dict[paper])
-		citation_detail[paper]["collaborative_citation"] = compute_collaborative_citation(paper_dict[paper],collab_set)
+	for author_name in author_name_list:
+		paper_folder_list = filter(lambda x:x not in file_black_list,listdir(author_name))
+		paper_dict = {}
+		for paper in paper_folder_list:
+			paper_dict[normalize_paper_name(paper)] = {}
+			citing_list = filter(lambda x:x not in file_black_list,listdir(author_name + "/" + paper))
+			for citing_paper in citing_list:
+				paper_dict[normalize_paper_name(paper)][normalize_paper_name(citing_paper)] = get_author_list(author_name + "/" + paper + "/" + citing_paper)
+		# pprint.pprint(paper_dict)
+		collab_set,side_author_dict = get_collaboration_set(author_name)
 
-	# print citation_detail
-	plot_graphs(citation_detail)
-	# print get_collaboration_set(author_name)
+		# print collab_set
+
+		# print side_author_dict
+		# pprint.pprint(side_author_dict.keys())
+		citation_detail[author_name] = {}
+		for paper in paper_dict:
+			citation_detail[author_name][paper] = {}
+			citation_detail[author_name][paper]["total_citation"] = len(paper_dict[paper])
+			try:
+				citation_detail[author_name][paper]["self_citation"] = compute_collaborative_citation(paper_dict[paper],side_author_dict[paper])
+			except Exception as e:
+				citation_detail[author_name][paper]["self_citation"] = -1
+				# print str(e)
+				# print 'in main loop = {' + paper + '}'
+			citation_detail[author_name][paper]["own_citation"] = compute_own_citation(author_name,paper_dict[paper])
+			citation_detail[author_name][paper]["collaborative_citation"] = compute_collaborative_citation(paper_dict[paper],collab_set)
+
+		# print citation_detail
+	plot_separate_graphs(citation_detail)
+	plot_together_graphs(citation_detail)
 	return
 
-analyze_author(author_list[1])
+analyze_author(author_list)
